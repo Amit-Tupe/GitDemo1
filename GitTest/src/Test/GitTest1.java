e683e75ebca133b29094951d572732c115e07519
AMITRAJE <AMITRAJE@AMITRAJE-PC>
2020-02-17 11:49:18 +0530

    Create m1 method

--- GitTest/src/Test/GitTest1.java
+++ GitTest/src/Test/GitTest1.java
@@ -2,6 +2,10 @@ package Test;
 
 public class GitTest1 {
 
+	public void m1()
+	{
+		System.out.println("This is m1 method");
+	}
 	public static void main(String[] args)
 	{
 		System.out.println("First Demo Git...");
